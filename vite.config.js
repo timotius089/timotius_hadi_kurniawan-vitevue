bf8150457ec195ee7784eaa3fe0da3a1255bc43d
Timotius <timotius089@gmail.com>
2024-12-04 14:24:09 +0700

    Add base

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: "/timotius_hadi_kurniawan-vitevue",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
